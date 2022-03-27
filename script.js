59a1831aa45a68f582b3c7a78ae98baa51e49d78
Alessandro Crinella <alecrinella99@gmail.com>
2022-03-27 15:07:06 +0200

    complete

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use script";
 
 const container = document.querySelector(".container");
+const reloadBtn = document.querySelector(".btn");
 
 function init() {
-  //const squareNumSide = prompt("How many squares do you want on your canvas?");
+  const squareNumSide = prompt("How many squares do you want on your canvas?");
 
   convertToGrid(squareNumSide);
 }
-
 init();
 
 function convertToGrid(squareNumSide) {
@@ -20,15 +20,44 @@ function convertToGrid(squareNumSide) {
 
 function styleSquare() {
   const square = document.createElement("div");
+  square.classList.add("square-hover");
 
   return square;
 }
 
+function genRandomNum() {
+  return Math.floor(Math.random() * (255 - 0 + 1));
+}
+
+function genRandomColor() {
+  let r = genRandomNum();
+  let g = genRandomNum();
+  let b = genRandomNum();
+
+  return `rgb(${r},${g},${b})`;
+}
+
 function createSquares(squarePerSide) {
   let totalSquares = squarePerSide * squarePerSide;
-  console.log(totalSquares);
 
   for (let i = 1; i <= totalSquares; i++) {
     container.appendChild(styleSquare());
   }
+
+  drawLine();
 }
+
+function drawLine() {
+  const squares = Array.from(document.querySelectorAll(".square-hover"));
+
+  squares.forEach((square) =>
+    square.addEventListener("mouseenter", function () {
+      square.style.backgroundColor = genRandomColor();
+    })
+  );
+}
+
+reloadBtn.addEventListener("click", function () {
+  container.innerHTML = "";
+  init();
+});
